a73e54bf3c75200bb5692c22cf6edcf5e1221485
ashishhaldia <ashishhaldia@hotmail.com>
2022-08-08 02:15:03 +0530

    User Api completed

--- app/user/serializer.py
+++ app/user/serializer.py
@@ -16,6 +16,16 @@ class UserSerializer(serializers.ModelSerializer):
 
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user , setting the password correctly an return"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for teh user authentication"""
