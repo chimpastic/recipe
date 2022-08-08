10f85bb0358b4a66b61b3b3eff6cd4f320536ad1
ashishhaldia <ashishhaldia@hotmail.com>
2022-08-09 01:37:10 +0530

    added list ingredients feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -54,3 +54,13 @@ class ModalTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredients_str(self):
+        """Test the ingredients string representation"""
+
+        ingredients = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredients), ingredients.name)
--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,4 +1,3 @@
-from os import stat
 from django.contrib.auth import get_user_model
 
 from django.urls import reverse
@@ -28,31 +27,44 @@ class PublicTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    class PrivateTagsApiTests(TestCase):
-        def setup(self):
-            self.user = get_user_model().objects.create_user('test@hotmail.com', 'testpass')
-            self.client = APIClient()
-            self.client.force_authenticate(self.user)
 
-        def test_retrieve_tags(self):
+class PrivateTagsApiTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('test@hotmail.com', 'testpass')
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
 
-            Tag.objects.create(user=self.user, name='Vegan')
-            Tag.objects.create(user=self.user, name='desserts')
+    def test_retrieve_tags(self):
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='desserts')
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-            res = self.client.get(TAGS_URL)
+    def test_tags_limited_to_user(self):
+        """Test taht tags returned are for the user"""
+        user2 = get_user_model().objects.create_user('other@hotmail.com', 'testpass')
+        Tag.objects.create(user=user2, name='fruity')
+        tag = Tag.objects.create(user=self.user, name='confort fruit')
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
 
-            tags = Tag.objects.all().order_by('-name')
-            serializer = TagSerializer(tags, many=True)
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(res.data, serializer.data)
+    def tets_create_tag_successful(self):
+        """Test creating a new Tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
 
-        def test_tags_limited_to_user(self):
-            """Test taht tags returned are for the user"""
-
-            user2 = get_user_model().objects.create_user('other@hotmail.com', 'testpass')
-            tag = Tag.objects.create(user=self.user, name='confort fruit')
-            res = self.client.get(TAGS_URL)
-
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(len(res.data), 1)
-            self.assertEqual(res.data[0]['name'], tag.name)
+    def test_create_tag_invalid(self):
+        """test creating a invalid tag"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
